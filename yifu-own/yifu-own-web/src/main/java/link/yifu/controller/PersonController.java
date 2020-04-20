d3bd0ac361d4b52c497894c8748f6358012d429e
zhuzongbao <zzb125125@163.com>
2020-04-20 16:51:04 +0800

    person加了控制台信息

--- yifu-own/yifu-own-web/src/main/java/link/yifu/controller/PersonController.java
+++ yifu-own/yifu-own-web/src/main/java/link/yifu/controller/PersonController.java
@@ -34,6 +34,7 @@ public class PersonController {
     @GetMapping("/getUser")
     public PersonEntity getUser(){
         PersonEntity entity = new PersonEntity();
+        System.out.println("1111111");
         entity.setId(1);
         entity.setName("xx");
         entity.setAge(28);
